8968999e7b9c3acbec1f710458ebcb1e264b9d3d
ofirl <ofir@bionic.ai>
2021-10-14 21:04:26 +0300

    all files

---
+++ src/Logic/redux/reducerSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+import initialState from "./initialState";
+import { accountReducers, themeReducers, topMenuReducers } from "./reducers"
+
+
+export const reducerSlice = createSlice({
+  name: "actions",
+  initialState: initialState,
+  reducers: {
+    ...accountReducers,
+    ...themeReducers,
+    ...topMenuReducers
+  },
+});
+
+export const {
+  setActiveUser,
+  setCurrentTab,
+  toggleCurrentThemeType,
+  setCurrentCollection,
+} = reducerSlice.actions;
+
+export default reducerSlice.reducer;
