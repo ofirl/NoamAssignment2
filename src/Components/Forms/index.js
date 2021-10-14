c63fdcaed6c670cdddcaaf5c2f2cdf6d47c5c50d
ofirl <ofir@bionic.ai>
2021-10-14 21:08:48 +0300

    Revert "all files"
    This reverts commit 8968999e7b9c3acbec1f710458ebcb1e264b9d3d.

--- src/Components/Forms/index.js
+++
@@ -1,9 +0,0 @@
-import BaseForm from './BaseForm';
-import Login from './Login';
-import Register from './Register';
-
-export {
-    BaseForm,
-    Login,
-    Register
-}
--- src/Logic/redux/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import reducerSlice from "./reducerSlice";
-
-export default configureStore({
-  reducer: { actions: reducerSlice },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware({
-    serializableCheck: false
-  })
-});
